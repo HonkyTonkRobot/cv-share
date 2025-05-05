4677c068035dd831afe56e413b4d770a951b0c7a
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-05-05 16:11:20 +1200

    style: center profile image

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,11 +1,13 @@
-import { Button, Container, Img } from "@chakra-ui/react"
+import { Button, Container, Flex, Img } from "@chakra-ui/react"
 import CvRequestForm from "./CvRequestForm"
 
 function App() {
   return (
     <>
       <header className="header">
-        <Img w={64} src="/public/images/honkytonk-robot-portrait.png" />
+        <Flex justifyContent='center'>
+          <Img w={64} src="/public/images/honkytonk-robot-portrait.png" />
+        </Flex>
       </header>
       <main>
         <Container >
