8dc3eff06bc5c2ea4601dab1865a1f0f6143b19d
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-05-13 11:31:44 +1200

    create api hook for adding prospect

---
+++ client/apis/use-add-prospect.ts
@@ -0,0 +1,15 @@
+import request from 'superagent'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { ProspectData } from '../../models/Prospect'
+
+export default function useAddProspect() {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: async (data: ProspectData) => {
+      await request.post('api/v1/prospect').send(data)
+    },
+    onSuccess: async () => {
+      queryClient.invalidateQueries({ queryKey: ['user'] })
+    },
+  })
+}
