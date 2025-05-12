f07a959607be1d6ceffde8616f8068df2cabc470
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-05-12 14:23:47 +1200

    feat: get Email/Linkedin Inputs rendering errors only after first blur event

--- client/components/CvRequestForm.tsx
+++ client/components/CvRequestForm.tsx
@@ -1,15 +1,15 @@
-import { Box, Button, FormControl, FormErrorMessage, FormLabel, Heading, Input, Text, VStack } from "@chakra-ui/react"
+import { Box, FormControl, FormErrorMessage, FormLabel, Heading, Input, Text, VStack } from "@chakra-ui/react"
 import { ChangeEvent, useState } from "react"
 
 // NOTE: How AI was used on this page
-// - to get the regex for email's
+// - to get the regex for email's and linkedin
 // - to learn what the .test() method did in JavaScript so that I can apply it, and understand what it is doing.
 
 function CvRequestForm() {
   const [emailInput, setEmailInput] = useState('')
-  // const [emailError, setEmailError] = useState('')
+  const [emailBlurred, setEmailBlurred] = useState(false)
   const [linkedInInput, setLinkedInInput] = useState('')
-  // const [linkedInError, setLinkedInError] = usetState('')
+  const [linkedInBlurred, setLinkedInBlurred] = useState(false)
 
   const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
     setEmailInput(e.target.value)
@@ -17,6 +17,14 @@ function CvRequestForm() {
 
   const handleLinkedInChange = (e: ChangeEvent<HTMLInputElement>) => {
     setLinkedInInput(e.target.value)
+  }
+
+  const handleEmailBlur = () => {
+    setEmailBlurred(true)
+  }
+
+  const handleLinkedInBlur = () => {
+    setLinkedInBlurred(true)
   }
 
   const emailRegex = new RegExp("^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$")
@@ -49,25 +57,27 @@ function CvRequestForm() {
         <FormLabel>Company</FormLabel>
         <Input placeholder='Company name' />
       </FormControl>
-      <FormControl isInvalid={!isEmail} >
+      <FormControl isInvalid={!isEmail && emailInput !== ''} >
         <FormLabel>Email</FormLabel>
-        <Input type='email' value={emailInput} placeholder='Email' onChange={handleEmailChange} />
-        {isEmail ? ""
+        <Input type='email' value={emailInput} placeholder='Email' onChange={handleEmailChange} onBlur={handleEmailBlur} />
+        {!isEmail && !emailBlurred ? ""
           : (
             <FormErrorMessage>
               Email is invalid
             </FormErrorMessage>
           )}
       </FormControl>
-      <FormControl isInvalid={!isLinkedIn} >
+      <FormControl isInvalid={!isLinkedIn && linkedInInput !== ''} >
         <FormLabel>LinkedIn</FormLabel>
-        <Input placeholder='LinkedIn' value={linkedInInput} onChange={handleLinkedInChange} />
-        {isLinkedIn ? ""
-          : (
-            <FormErrorMessage>
-              Linkedin is invalid :(
-            </FormErrorMessage>
-          )}
+        <Input placeholder='LinkedIn' value={linkedInInput} onChange={handleLinkedInChange} onBlur={handleLinkedInBlur} />
+        {
+          !isLinkedIn && !linkedInBlurred ? ""
+            : (
+              <FormErrorMessage>
+                Linkedin is invalid :(
+              </FormErrorMessage>
+            )
+        }
       </FormControl>
       {/* <Button mt={4} type='submit'>Submit</Button> */}
       {/* </form> */}
