8f13edb04142fbc742def27f47e0b4de6388951a
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-05-13 11:32:16 +1200

    generate knex migration file for prospects table

---
+++ server/db/migrations/20250512232322_prospects.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('prospects', (table) => {
+    table.integer('id').primary()
+    table.string('name')
+    table.string('company')
+    table.string('email')
+    table.string('linkedin')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable('prospects')
+}
