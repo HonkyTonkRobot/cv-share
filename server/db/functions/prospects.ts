6e27fe10d0b434d7ce0db3968b0154a196d2304e
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-05-13 13:25:19 +1200

    add db function for prospect post route

---
+++ server/db/functions/prospects.ts
@@ -0,0 +1,17 @@
+import db from './../connection.ts'
+import { ProspectData } from '../../../models/Prospect.ts'
+
+//NOTE: AI Acknowledgment
+//when double checking if db functions could pass undefined or null values, it recommend giving it a default value so that empty values would be consistent, and recommend using the nullish coalescing operator.
+
+// CREATE
+
+export async function addNewProspect(data: ProspectData) {
+  const userAdded = await db('prospects').insert({
+    name: data.name,
+    company: data.company,
+    email: data.email ?? null,
+    linkedin: data.linkedin ?? null,
+  })
+  return userAdded
+}
