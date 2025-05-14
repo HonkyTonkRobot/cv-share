4b7e0cb12a4f4c338eb79c373637734bf40e48a1
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-05-14 15:39:52 +1200

    get Download CV link working an rendering to component

--- client/components/CvDownload.tsx
+++ client/components/CvDownload.tsx
@@ -1,8 +1,10 @@
+import { Link } from "@chakra-ui/react"
 
 function CvDownload() {
 
   return (
-    <p>temp</p>
+    <Link href="https://raw.githubusercontent.com/HonkyTonkRobot/cv-share/b38eb5f667464858d2c6259571eff89a39515de3/public/cv-joel-anderson-1-page.pdf
+" download color="blue.500" fontSize="lg" >Download CV</Link>
   )
 
 }
